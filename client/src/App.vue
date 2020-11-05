4df4216455183a13820553eed3072c69c9d43d84
Cade Li <cade.li@qq.com>
2020-11-05 14:37:52 +0800

    [Feature]添加简单页面布局

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <MyHeader></MyHeader>
+        <PageBord></PageBord>
+        <SideBar></SideBar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import MyHeader from "@/components/MyHeader";
+    import PageBord from "@/components/PageBord";
+    import SideBar from "@/components/SideBar";
+
+    export default {
+        name: 'App',
+        components: {
+            MyHeader,
+            PageBord,
+            SideBar
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        height: 100%;
+    }
 </style>
---
+++ client/src/components/MyHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="my-header">MyHeader</header>
+</template>
+
+<script>
+    export default {
+        name: "MyHeader"
+    }
+</script>
+
+<style>
+.my-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    width: 100%;
+    height: 60px;
+    background-color: #000;
+    color: #fff;
+}
+</style>
