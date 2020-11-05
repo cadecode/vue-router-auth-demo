7fafea84eb5697c23ba0966dcf23d3e1bab9fbb6
Cade Li <cadecode@foxmail.com>
2020-11-05 14:37:52 +0800

    [Feature]添加简单页面布局

---
+++ client/src/components/PageBord.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="page-bord">PageBord</div>
+</template>
+
+<script>
+    export default {
+        name: "PageBord"
+    }
+</script>
+
+<style>
+.page-bord{
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    padding: 90px 30px 30px 230px;
+    background-color: #ddd;
+}
+</style>
---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="side-bar">SideBar</div>
+</template>
+
+<script>
+    export default {
+        name: "Sidebar"
+    }
+</script>
+
+<style>
+    .side-bar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        width: 200px;
+        height: 100%;
+        padding-top: 90px;
+        box-sizing: border-box;
+        background-color: #999;
+    }
+</style>
