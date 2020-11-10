b47932804f6408752a85bdd4010fd0c6a00c08d2
Cade Li <cade.li@qq.com>
2020-11-10 22:46:59 +0800

    [Feature]新增 util.js generateRoutes 函数，将树形数据转化为路由

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,22 +1,33 @@
-function formatRouteTree(data) {
-    
-    function dataToTree(data, pid) {
-        const result = []
-        data.forEach(item => {
-            if (item.pid === pid) {
-                const tree = dataToTree(data, item.id)
-                if (tree.length > 0) {
-                    item.children = tree
-                }
-                result.push(item)
+function formatRouteTree(data, pid = 0) {
+    const result = []
+    data.forEach(item => {
+        if (item.pid === pid) {
+            const tree = formatRouteTree(data, item.id)
+            if (tree.length > 0) {
+                item.children = tree
             }
-        })
-        return result
-    }
-    
-    return dataToTree(data, 0);
+            result.push(item)
+        }
+    })
+    return result;
+}
+
+function generateRoutes(userRoutes) {
+    const newRoutes = userRoutes.map(r => {
+        const routes = {
+            path: r.path,
+            name: r.name,
+            component: () => import(`@/views/${r.name}`)
+        }
+        if (r.children) {
+            routes.children = generateRoutes(r.children)
+        }
+        return routes
+    })
+    return newRoutes
 }
 
 export {
-    formatRouteTree
+    formatRouteTree,
+    generateRoutes
 }
---
+++ client/src/store/mutations.js
@@ -0,0 +1,8 @@
+export default {
+    setAuth(state, auth) {
+        state.hasAuth = auth
+    },
+    setUserRoutes(state, userRoutes) {
+        state.userRoutes = userRoutes
+    }
+}
