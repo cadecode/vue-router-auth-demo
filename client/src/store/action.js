c9af8ec9ce491c0d9ba2aa7310dd484bf833ebd6
Cade Li <cadecode@foxmail.com>
2020-11-10 22:46:59 +0800

    [Feature]新增 util.js generateRoutes 函数，将树形数据转化为路由

--- client/src/store/action.js
+++ client/src/store/action.js
@@ -1,10 +1,12 @@
 import {getUserRoutes} from '@/services'
-import {formatRouteTree} from '@/lib/utils'
+import {formatRouteTree, generateRoutes} from '@/lib/utils'
 
 export default {
-    async setUserRoutes({state}) {
+    async setUserRoutes({commit, state}) {
         const userRoutes = await getUserRoutes(state.uid)
         const payload = formatRouteTree(userRoutes)
-        console.log(payload)
+        console.log(generateRoutes(payload))
+        commit('setUserRoutes', payload)
+        commit('setAuth', true)
     }
 }
