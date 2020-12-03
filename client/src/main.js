e0c5663ca48b6883d56f793e951d4c2686750fa4
Cade Li <cade.li@qq.com>
2020-12-03 10:31:13 +0800

    [Feature]添加路由守卫，beforeEach 中判断 hasAuth，请求权限路由

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,21 @@ import App from './App.vue'
 import store from './store'
 import router from '@/router'
 import '@/assets/css/common.css'
+import {generateRoutes} from '@/lib/utils'
+
+router.beforeEach(async (to, from, next) => {
+    if (!store.state.hasAuth) {
+        await store.dispatch("setUserRoutes")
+        const newRoutes = generateRoutes(store.state.userRoutes)
+        console.log(newRoutes)
+        router.addRoutes(newRoutes)
+        next({path: to.path})
+    } else {
+        next()
+    }
+})
 
 Vue.config.productionTip = false
-
-store.dispatch('setUserRoutes').then()
 
 new Vue({
     store,
--- client/src/store/action.js
+++ client/src/store/action.js
@@ -1,11 +1,10 @@
 import {getUserRoutes} from '@/services'
-import {formatRouteTree, generateRoutes} from '@/lib/utils'
+import {formatRouteTree} from '@/lib/utils'
 
 export default {
     async setUserRoutes({commit, state}) {
         const userRoutes = await getUserRoutes(state.uid)
         const payload = formatRouteTree(userRoutes)
-        console.log(generateRoutes(payload))
         commit('setUserRoutes', payload)
         commit('setAuth', true)
     }
